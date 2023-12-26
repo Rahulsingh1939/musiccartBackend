cd13f6528b1c7c2be5d894e51d24b2c81c6852f5
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-12-26 21:56:29 +0530

    Working on Protected Routes

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+const JWT = require('jsonwebtoken');
+
+//Token Based Authentication
+const JWT_SECRET = process.env.JWT_SECRET;
+const requireSignIn = async (req,res,next) => {
+    try {
+        const decodedUser=JWT.verify(req.headers.authorization,JWT_SECRET);
+        req.user=decodedUser;
+        next();
+    } catch (error) {
+        console.log(error)
+        res.status(403).send({
+            success:false,
+            message:'Invalid Token'
+        })
+    }
+
+}
+
+module.exports ={requireSignIn};
