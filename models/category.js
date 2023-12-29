d88fb11fd003476000909e80cb8fd89c378146e4
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-12-29 18:04:08 +0530

    Get All Products Done

--- models/category.js
+++ models/category.js
@@ -1,15 +1,15 @@
-const mongoose  = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    slug:{
-        type:String,
-        lowercase:true
-    }
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  slug: {
+    type: String,
+    lowercase: true,
+  },
 });
-const categoryModel = mongoose.model('Category', categorySchema);
+const categoryModel = mongoose.model("Category", categorySchema);
 module.exports = categoryModel;
