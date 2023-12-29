53465c965d253d87d6c7afc6ffe60b3e92db2ce8
Rahulsingh1939 <rahulsingh19390@gmail.com>
2023-12-29 22:00:07 +0530

    Get Single Products Done

--- controller/productController.js
+++ controller/productController.js
@@ -5,10 +5,10 @@ const createProduct = async (req, res) => {};
 //Get All Products
 const getAll = async (req, res) => {
   try {
-    const products = await productModel.find({});
+    const products = await productModel.find({}).populate("category");
     res.status(200).send({
       success: true,
-      productCount:products.length,
+      productCount: products.length,
       message: "All Products List",
       products,
     });
@@ -22,4 +22,23 @@ const getAll = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, getAll };
+const getProduct = async (req, res) => {
+  try {
+    const product = await productModel
+      .findOne({ slug: req.params.slug })
+      .populate("category");
+    res.status(200).send({
+      success: true,
+      message: "Got Item Successfully",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While Getting The Product",
+    });
+  }
+};
+module.exports = { createProduct, getAll, getProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const { createProduct, getAll } = require("../controller/productController");
+const { createProduct, getAll, getProduct } = require("../controller/productController");
 
 const router = express.Router();
 
 // Create a new Product Route
 router.post("/create-product", createProduct);
+
+// Get Single Product
+router.get("/product/:slug",getProduct);
 
 //Get all Products Route
 router.get("/all-products", getAll);
